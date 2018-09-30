455023d596a54805ef3b7584a4a9f6d83d33ddc3
Jason Bao <jzbao@fb.com>
2018-09-30 16:15:31 -0700

    populated artist columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_230635) do
+ActiveRecord::Schema.define(version: 2018_09_30_231452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2018_09_30_230635) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "program"
+    t.string "name"
+    t.boolean "open_to_commissions"
     t.index ["email"], name: "index_artists_on_email", unique: true
     t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true
   end
