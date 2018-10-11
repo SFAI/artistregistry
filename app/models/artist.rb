1979241fd365e32f1fb401fd90db6f7859d48895
Alison Dowski <adowski@berkeley.edu>
2018-10-11 15:45:15 -0700

    added artist, buyer, and commissions relationships

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,5 @@ class Artist < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :buyers, through: :commisions
 end
