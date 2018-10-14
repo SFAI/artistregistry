744ac98d1d18b0d14b8919fb9265003698ba1c24
Jason Bao <bao.jason.z@gmail.com>
2018-10-14 16:45:29 -0700

    switching branches rip

---
+++ app/assets/javascripts/components/artists/SingIn.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+      	hi bye 
+      </div>
+    )
+  }
+}
