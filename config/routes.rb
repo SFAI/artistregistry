f2d92ef5db2f4e4b24b5704f1d0b23a81ba05e89
Calvin Dong <calvind777@gmail.com>
2018-10-18 19:39:26 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'artists/:id' => 'artists#show'
+  
   devise_for :artists
   devise_for :buyers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-
+  get 'artists/:id' => 'artists#show'
 
 
   namespace :api, defaults: { format: :json } do
