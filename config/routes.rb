613274bcb40fecfcec7c72672ef0d2857a3e6bb3
Claire Lin <clairelin135@gmail.com>
2018-10-24 12:12:32 -0700

    add buyer controller and modify nav

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   devise_for :buyers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  get 'artists/:id' => 'artists#show'
-
+  get 'artists/:id' => 'artists#show', as: :artistid
   get '/artists/', to: 'artists#all_artists'
+
+  get 'buyers/:id' => 'buyers#show', as: :buyerid
 
   namespace :api, defaults: { format: :json } do
     resources :works, :only => [:show, :create, :update, :destroy]
