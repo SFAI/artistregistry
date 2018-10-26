2e53513889f9ee5fd45823f4b3c98fd4cd6eca75
Claire Lin <clairelin135@gmail.com>
2018-10-25 17:19:39 -0700

    fix merge conflict error

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :works, :only => [:show, :create, :update, :destroy]
-    resources :artists, :only => [:show, :create, :update, :destroy]
+    resources :artists, :only => [:index, :show, :create, :update, :destroy]
     resources :requests, :only => [:show, :create, :update]
     get 'artists/works/:id' => 'artists#works'
     get 'artists/requests/:id' => 'artists#requests'
