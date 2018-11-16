36bca91cd68faff1df87c811131f85bea5e741c8
Claire Lin <clairelin135@gmail.com>
2018-11-16 15:11:55 -0800

    update validator, prepopulate login fields

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -3,7 +3,7 @@ class EmailValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
     begin
       m = Mail::Address.new(value)
-      r = m.domain.present? && m.domain.match('^artists\.sfai\.edu$') && m.address == value
+      r = m.domain.present? && (m.domain.match('^artists\.sfai\.edu$') || m.domain.match('^alumni\.sfai\.edu$')) && m.address == value
     rescue
       r = false
     end
