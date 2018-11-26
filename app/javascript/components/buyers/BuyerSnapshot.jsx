131a06918503f030f26a87af232b739322182117
Claire Lin <clairelin135@gmail.com>
2018-11-25 23:26:24 -0800

    skeleton for buyer snapshot

---
+++ app/javascript/components/buyers/BuyerSnapshot.jsx
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+class BuyerSnapshot extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+
+export default BuyerSnapshot;
