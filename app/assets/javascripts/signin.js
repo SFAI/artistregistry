955b11736d297db8e071d4bed6340a67a02b0a2f
Emily Zhong <emilyzhongsihua@gmail.com>
2019-02-28 20:30:23 -0800

    dubious client-side validation on first page but ok

--- app/assets/javascripts/signin.js
+++ app/assets/javascripts/signin.js
@@ -1,4 +1,56 @@
-nextPage = () => {
-	$(".signup.part1").hide();
-	$(".signup.part2").show();
+nextPage = (e) => {
+	if (validateForm()) {
+		$(".signup.part1").hide();
+		$(".signup.part2").show();
+	} else {
+	}
+}
+
+checkInputs = () => {
+	let isCorrect = true;
+	$('.part1 input').each(function() {
+	  if ($(this).val().length <= 1) {
+	  	isCorrect = false;
+	  }
+	});
+
+	return isCorrect;
+}
+
+validateForm = () => {
+	let valid = true;
+
+	const email = $('input[name="artist[email]"]');
+	const password = $('input[name="artist[password]"]');
+	const passwordConfirmation = $('input[name="artist[password_confirmation]"]');
+
+	const atIndex = email.val().indexOf("@");
+	if (!email.val() ||
+			atIndex == -1 ||
+			email.val().slice(atIndex + 1, email.val().length) != "artists.sfai.edu") {
+		email.addClass("invalid");
+		valid = false;
+	}
+
+	if (!password.val() || password.val().length < 6) {
+		password.addClass("invalid");
+		valid = false;
+	}
+
+	if (!passwordConfirmation.val() ||
+		passwordConfirmation.val().length < 6) {
+		passwordConfirmation.addClass("invalid");
+		valid = false;
+	}
+
+	if (password.val() != passwordConfirmation.val()) {
+		passwordConfirmation.addClass("invalid");
+		valid = false;
+	}
+
+  return valid; 
+}
+
+resetStyle = (el) => {
+	$(el).removeClass("invalid");
 }
