0484f972fc5b04ee5b1f30b1930de92852e22b7e
Amy Shan <amyshan@berkeley.edu>
2019-03-03 20:45:29 -0800

    changed dev host to localhost instead of heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => 'calblueprint-sfai.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
