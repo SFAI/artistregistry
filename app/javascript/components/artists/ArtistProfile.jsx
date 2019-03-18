3109d651952774d5b9c3876e1bcd7b7c8c242bf9
Tommy Poa <tommypoa@berkeley.edu>
2019-03-18 00:29:42 -0700

    Functionality of unhide present, along with button.

--- app/javascript/components/artists/ArtistProfile.jsx
+++ app/javascript/components/artists/ArtistProfile.jsx
@@ -123,6 +123,23 @@ class ArtistProfile extends React.Component {
     });
   }
 
+  unHideWork = (work_id) => {
+    let formData = new FormData();
+    formData.append(`work[hidden]`, false);
+    fetch(APIRoutes.works.update(work_id), {
+      method: 'PUT',
+      body: formData,
+      credentials: 'same-origin',
+      headers: {
+        "X_CSRF-Token": document.getElementsByName("csrf-token")[0].content
+      }
+    }).then((data) => {
+      window.location = `/artists/` + this.props.artist.id;
+    }).catch((data) => {
+      console.error(data);
+    });
+  }
+
   render() {
     const { componentDidMount, activeFilter, artist, works, canEditProfile } = this.state;
     const { name, program, media, description } = artist;
@@ -199,10 +216,18 @@ class ArtistProfile extends React.Component {
                         <FontAwesomeIcon className="white" icon={faTrash} />
                         <h4 className="ml2 white">Delete</h4>
                       </Button>
+                      {work.hidden == false &&
                       <Button className="ml2" type="hover-button" onClick={() => this.hideWork(work.id)}>
                         <FontAwesomeIcon className="white" icon={faTrash} />
                         <h4 className="ml2 white">Hide</h4>
                       </Button>
+                      }
+                      {work.hidden == true &&
+                      <Button className="ml2" type="hover-button" onClick={() => this.unHideWork(work.id)}>
+                        <FontAwesomeIcon className="white" icon={faTrash} />
+                        <h4 className="ml2 white">Unhide</h4>
+                      </Button>
+                      }
                     </div>
                   }
                 </WorkColumnPanel>
