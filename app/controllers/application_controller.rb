9291787e51773179a8eb841d23595c7cd83cc4fc
Tommy Poa <tommypoa@berkeley.edu>
2019-03-18 01:27:18 -0700

    Changes to current_user things. Not entirely sure but pattern matched to make sure it was correct for admin user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,17 @@ class ApplicationController < ActionController::Base
   def toast(type, text)
     flash[:toastr] = { type => text }
   end
-  
+
   def set_current_user
     if current_artist
       @current_user = current_artist
       @current_user_type = "artist"
-    else
+    else if current_buyer
       @current_user = current_buyer
       @current_user_type = "buyer"
+    else
+      @current_user = current_admin
+      @current_user_type = "admin"
     end
   end
 
