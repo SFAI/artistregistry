8335c16892fb6fc0c0e390c940204d1398f8baf2
Barbara Yang <by@berkeley.edu>
2019-03-18 20:10:08 -0700

    Change redirect link after logging in

--- app/controllers/buyers/registrations_controller.rb
+++ app/controllers/buyers/registrations_controller.rb
@@ -64,7 +64,7 @@ class Buyers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    '/buyers/confirm_email'
+  end
 end
