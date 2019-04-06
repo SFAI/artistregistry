5286452b0e9359ca158108f427c5dd95fa6d2ad6
Thu Nguyen <tmnguyen@berkeley.edu>
2019-04-06 14:26:20 -0700

    changes not done yet tho

--- app/javascript/components/works/WorkColumnPanel.jsx
+++ app/javascript/components/works/WorkColumnPanel.jsx
@@ -19,24 +19,26 @@ class WorkColumnPanel extends React.Component {
   render() {
     let work = this.props.work;
     return (
-      <div className="mb3 pa3 w-100 col-item bg-white relative" key={work.id}>
-        <div className="item-overlay">
-          {this.props.children}
-        </div>
-        {work.featured_image &&
+      <div className="">
+        <div className="mb3 pa3 w-100 col-item bg-white relative" key={work.id}>
+          <div className="item-overlay">
+            {this.props.children}
+          </div>
+          {work.featured_image &&
+            <Touchable onPress={() => this.navigateToWork(work.id)}>
+              {<img src={work.featured_image.url} className="mb3 pointer" />}
+            </Touchable>
+          }
           <Touchable onPress={() => this.navigateToWork(work.id)}>
-            {<img src={work.featured_image.url} className="mb3 pointer" />}
+            <h3 className="pointer">{work.title}</h3>
           </Touchable>
-        }
-        <Touchable onPress={() => this.navigateToWork(work.id)}>
-          <h3 className="pointer">{work.title}</h3>
-        </Touchable>
-        {!this.props.hideArtistName &&
-          <Touchable onPress={() => this.navigateToArtist(work.artist_id)}>
-            <h5 className="pointer magenta">{work.artist_name}</h5>
-          </Touchable>
-        }
-        <h6>{work.material}</h6>
+          {!this.props.hideArtistName &&
+            <Touchable onPress={() => this.navigateToArtist(work.artist_id)}>
+              <h5 className="pointer magenta">{work.artist_name}</h5>
+            </Touchable>
+          }
+          <h6>{work.material}</h6>
+        </div>
       </div>
     );
   }
