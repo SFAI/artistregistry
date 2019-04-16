67ee1eced82a9b1b8b7a89811af3cbdd021b99db
Tommy Poa <tommypoa@berkeley.edu>
2019-04-15 19:48:11 -0700

    Fixed work form syntax error.

--- app/controllers/api/works_controller.rb
+++ app/controllers/api/works_controller.rb
@@ -97,7 +97,6 @@ class Api::WorksController < ApplicationController
   end
 
   def index
-    # works = Work.all
     @work_count = Work.count
     works = Work.page(params[:page])
     render json: works,
