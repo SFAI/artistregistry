cd183244d37051a358bcc9de9612645682a3fd9c
Tommy Poa <tommypoa@berkeley.edu>
2019-05-05 02:36:17 -0700

    Implemented.

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -31,40 +31,29 @@ class Api::ArtistsController < ApplicationController
   def filtered_artists
     parsed_query = CGI.parse(params[:search_params])
     all_artists = Artist.all
+    filtered_program_artists = Artist.all
     filtered_artists = Artist.all
-
     filter_categories = parsed_query.keys
     for category in filter_categories
       if category == "program"
         query_programs = parsed_query["program"]
-        filtered_artists = Artist.where(["program @> ?", "{#{query_programs[0]}}"])
+        filtered_program_artists = Artist.where(["program @> ?", "{#{query_programs[0]}}"])
         for programs in parsed_query["program"][1..query_programs.length]
-          @programs = programs
-          filtered_artists = filtered_artists.or(all_artists.where(["program @> ?", "{#{@programs}}"]))
+          filtered_program_artists = filtered_program_artists.or(all_artists.where(["program @> ?", "{#{programs}}"]))
         end
       else
         filters = parsed_query[category]
         filtered_artists = filtered_artists.where(["#{category}=?", Artist.degrees[filters[0]]])
         for filter in filters[1..filters.length]
-          @filter = filter
-          filtered_artists = filtered_artists.or(all_artists.where(["#{category}=?", Artist.degrees[@filter]]))
+          filtered_artists = filtered_artists.or(all_artists.where(["#{category}=?", Artist.degrees[filter]]))
         end
       end
     end
-
-    # if "program".in?(parsed_query.keys)
-    #   filtered_artists = Artist.all.select{|artist| ((artist.program & parsed_query.values[0]).length > 0) }
-    #   if (parsed_query.keys.length > 1)
-    #     for key in parsed_query.keys
-    #       if key != "program"
-    #         filtered_artists = filtered_artists.select{|artist| (artist[key] == parsed_query[key][0])}
-    #       end
-    #     end
-    #   end
-    # end
-    # filtered_artists = current_admin ? 
-    #   Artist.where(parsed_query) : 
-    #   Artist.where(parsed_query).joins(:works).group('artists.id, works.hidden').where("works.hidden=false").where("artists.hidden=false")
+    filtered_artists = filtered_artists.merge(filtered_program_artists)
+    
+    filtered_artists = current_admin ? 
+      filtered_artists : 
+      filtered_artists.joins(:works).group('artists.id, works.hidden').where("works.hidden=false").where("artists.hidden=false")
     filtered_artists_page = filtered_artists.page(params[:page])
     artist_count = filtered_artists.length
     render json: {
