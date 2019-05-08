0cd5d0a3eda3e665fc03d95af050b77ea2a39be3
Barbara Yang <by@berkeley.edu>
2019-05-08 12:57:53 -0700

    Remove edit button for patrons

--- app/javascript/components/requests/Receipt.jsx
+++ app/javascript/components/requests/Receipt.jsx
@@ -107,12 +107,14 @@ class Receipt extends React.Component {
             <h5 className="mv0">Date Requested</h5>
             <p>{new Date(created_at).toLocaleDateString()}</p>
           </div>
-          <div className="mh3">
-            <button className="request-ellipsis ml3 br-100 pa0 pointer bn" />
-            <ul className="request-dropdown ml3 absolute nowrap z-3">
-              <li>{this.renderEditReceipt()}</li>
-            </ul>
-          </div>
+          {this.props.artist && (
+            <div className="mh3">
+              <button className="request-ellipsis ml3 br-100 pa0 pointer bn" />
+              <ul className="request-dropdown ml3 absolute nowrap z-3">
+                <li>{this.renderEditReceipt()}</li>
+              </ul>
+            </div>
+          )}
         </div>
 
         <div className="ttu b mt3 ml3 f6">{this.renderStatus()}</div>
