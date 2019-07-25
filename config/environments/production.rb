081c437955ff13c590d15b5f7866618a015d7974
tm jhnsn <2293335+tcj@users.noreply.github.com>
2019-07-25 16:21:25 -0700

    changed URL for production in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { :host => 'calblueprint-sfai.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'calblueprint-sfai.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'registry.sfai.edu' }
+  Rails.application.routes.default_url_options[:host] = 'registry.sfai.edu'
 
   config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
