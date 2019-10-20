1c426fd98137c26c52a711b6f79b7c79be8b80bb
Claire Lin <clairelin135@gmail.com>
2019-10-19 17:58:33 -0700

    upgrade dependencies & ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,8 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.force_ssl = true
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
